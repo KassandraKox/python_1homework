1871a561b1f1fd0605eced7c580710fed2f4933d
Anastasia <sawiii@mail.ru>
2022-10-28 10:12:37 +0300

    Конъюнкция и дизъюнкция

--- task2.py
+++ task2.py
@@ -1,9 +1,11 @@
 # Напишите программу для проверки истинности утверждения
 #  ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений 
 # предикат.
-X = int(input("Введи значение X: "))
-Y = int(input("Введи значение Y: "))
-Z = int(input("Введи значение Z: "))
-A = not(X or Y or Z)
-B = not(X) and not(Y) and not(Z)
-print(A, B)
+for X in [0, 1]:
+    for Y in [0, 1]:
+        for Z in [0, 1]:
+            print(X, Y, Z)
+            if not (X or Y or Z) == (not X and not Y and not Z):
+                print('Истина')
+            else:
+                print('Ложь')
