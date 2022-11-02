9e3893c5c1ea668f3c2c684e9928aadf51b88b10
Anastasia <sawiii@mail.ru>
2022-11-02 10:57:08 +0300

    Многочлен степени К

--- task4.4.py
+++ task4.4.py
@@ -6,16 +6,21 @@ k=2 => 2x^2 + 4x + 5 = 0 или x^2 + 5 = 0 или 10x^2 = 0'''
 
 import random
 
-A = random.randint(0, 100)
-B = random.randint(0, 100)
-C = random.randint(0, 100)
-k = 2
+random.seed
+k = int(input('Введите степень k:'))
+if k<0:
+    print('Некорректный ввод')
+else:
 
-with open("Task4.txt", "w") as Task4:
-    print(f'A = {A}, B = {B}, C = {C}', file=Task4)
-    if B != 0 or C != 0:
-        print(f'{A}*x^{k} + {B}*x + {C} = 0', file=Task4)
-    if B == 0 and C != 0:
-        print(f'{A}*x^{k} + {C} = 0', file=Task4)
-    if B == 0 and C == 0:
-        print(f'{A}*x^{k} = 0', file=Task4)
+    lst = []
+    for i in range(k+1):
+        lst.append(random.randint(1, 101))
+    print(f'Сгенерирован список коэффициентов: {lst}')
+    for i in range(k,0,-1):
+        print(f'{lst[i]}*x**{i} + ', end='')
+    print(f'{lst[0]}')
+
+    with open('task4.txt','w') as data:
+        for i in range(k,0,-1):
+            data.write(f'{lst[i]}*x**{i} + ')
+        data.write(f'{lst[0]}\n')
